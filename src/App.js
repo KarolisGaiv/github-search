1a78a109eda0ed0e9ef3888f77f2ac5b90da65b6
KarolisGaiv <kgaivenis@gmail.com>
2022-01-31 19:31:58 +0200

    Add basic header styling and content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import './styles/main.scss';
+import { ReactComponent as Moon } from './assets/icon-moon.svg';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+    <div className='content'>
+      <header className='header'>
+        <h1 className='header__title'>devfinder</h1>
+        <button className='header__page-style-button'>
+          <h4 className='header__page-style-name'>dark</h4>
+          <span className='header__icon-container'>
+            <Moon />
+          </span>
+        </button>
       </header>
     </div>
   );
