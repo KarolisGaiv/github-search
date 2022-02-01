a147d5b6059812367abcab613199c642e2b531cf
KarolisGaiv <kgaivenis@gmail.com>
2022-02-01 20:18:37 +0200

    Resize avatar

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -5,9 +5,11 @@ function UserProfile({ userData }) {
   return (
     <main className='user'>
       <header className='user__header'>
-        {/* <span className='user__avatar'>
-          <img src={userData.avatar_url} alt='user avatar' />
-        </span>{' '} */}
+        <img
+          src={userData.avatar_url}
+          alt='user avatar'
+          className='user__header__avatar'
+        />
         <div className='user__details-container'>
           <h1 className='user__details-container__name'>{userData.name}</h1>
           <h3 className='user__details-container__login'>@{userData.login}</h3>
@@ -15,48 +17,49 @@ function UserProfile({ userData }) {
             {userData.created_at}
           </p>
         </div>
-        <p className='user__biography'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-          deserunt odit ex tempora quasi modi, quaerat ea atque nisi deleniti.
-          {/* {userData.bio ? userData.bio : 'This profile has no bio'} */}
-        </p>
-        <div className='user__statistics-wrapper'>
-          <div className='user__statistics-wrapper__repo-details'>
-            <p className='user__statistics-wrapper__detail-header'>Repos</p>
-            <p className='user__statistics-wrapper__number'>
-              {userData.public_repos}
-            </p>
-          </div>
-          <div className='user__statistics-wrapper__repo-details'>
-            <p className='user__statistics-wrapper__detail-header'>Followers</p>
-            <p className='user__statistics-wrapper__number'>
-              {userData.followers}
-            </p>
-          </div>
-          <div className='user__statistics-wrapper__repo-details'>
-            <p className='user__statistics-wrapper__detail-header'>Following</p>
-            <p className='user__statistics-wrapper__number'>
-              {userData.following}
-            </p>
-          </div>
-        </div>
-        <ul className='user__contact-details'>
-          <li>
-            <p>{userData.location}</p>
-          </li>
-          <li>
-            <a href={userData.blog}>{userData.blog}</a>
-          </li>
-          <li>
-            <a href={userData.twitter_username}>{userData.twitter_username}</a>
-          </li>
-          <li>
-            <a href={`https://github.com/${userData.company}`}>
-              {userData.company}
-            </a>
-          </li>
-        </ul>
       </header>
+      <p className='user__biography'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+        deserunt odit ex tempora quasi modi, quaerat ea atque nisi deleniti.
+        {/* {userData.bio ? userData.bio : 'This profile has no bio'} */}
+      </p>
+      <div className='user__statistics-wrapper'>
+        <div className='user__statistics-wrapper__repo-details'>
+          <p className='user__statistics-wrapper__detail-header'>Repos</p>
+          <p className='user__statistics-wrapper__number'>
+            {userData.public_repos}
+          </p>
+        </div>
+        <div className='user__statistics-wrapper__repo-details'>
+          <p className='user__statistics-wrapper__detail-header'>Followers</p>
+          <p className='user__statistics-wrapper__number'>
+            {userData.followers}
+          </p>
+        </div>
+        <div className='user__statistics-wrapper__repo-details'>
+          <p className='user__statistics-wrapper__detail-header'>Following</p>
+          <p className='user__statistics-wrapper__number'>
+            {userData.following}
+          </p>
+        </div>
+      </div>
+
+      <ul className='user__contact-details'>
+        <li>
+          <p>{userData.location}</p>
+        </li>
+        <li>
+          <a href={userData.blog}>{userData.blog}</a>
+        </li>
+        <li>
+          <a href={userData.twitter_username}>{userData.twitter_username}</a>
+        </li>
+        <li>
+          <a href={`https://github.com/${userData.company}`}>
+            {userData.company}
+          </a>
+        </li>
+      </ul>
     </main>
   );
 }
