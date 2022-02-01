50a66b510e3f14057b5f1508eaf68227665cbbdf
KarolisGaiv <kgaivenis@gmail.com>
2022-02-01 19:30:54 +0200

    Add funcionality to fetch data default user profile data from API endpoint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import './styles/main.scss';
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 import UserProfile from './components/UserProfile/UserProfile';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('octocat');
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(`https://api.github.com/users/${searchQuery}`);
+    const data = await response.json();
+    setUserData(data);
+  };
+
   return (
     <div>
       <Header />
       <SearchBar />
-      <UserProfile />
+      <UserProfile userData={userData} />
     </div>
   );
 }
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,12 +1,20 @@
 import './userProfile.scss';
 import React from 'react';
 
-function UserProfile() {
+function UserProfile({ userData }) {
   return (
     <main className='user'>
       <header className='user__header'>
-        <span className='user__avatar'></span>
-        <div className='user__details-container'></div>
+        {/* <span className='user__avatar'>
+          <img src={userData.avatar_url} alt='user avatar' />
+        </span>{' '} */}
+        <div className='user__details-container'>
+          <h1 className='user__details-container__name'>{userData.name}</h1>
+          <h3 className='user__details-container__login'>@{userData.login}</h3>
+          <p className='user__details-container__joined-data'>
+            {userData.created_at}
+          </p>
+        </div>
         <p className='user__biography'></p>
         <div className='user__statistics-wrapper'></div>
         <div className='user__contact-details'></div>
